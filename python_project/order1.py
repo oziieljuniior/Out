1a01455ac89ce182ec1bff2b0d9b59f32efac788
Oziel Junior <58310583+oziieljuniior@users.noreply.github.com>
2024-05-25 20:21:08 -0400

    Att

--- python_project/order1.py
+++ python_project/order1.py
@@ -9,8 +9,24 @@ lista_entradas = []
 
 #Controladores do jogo.
 apostar = 0
-win = 0
+contagem = 0
 level = 1
+
+#Informação inicial que deve ser salva em uma planilha, considera-se como:
+#Rodada ~ Em que entrada o jogo está; level ~ Em qual level o jogador está; apostar ~ condição para apostar(0 - Não apostar, 1 - apostar)
+#Acerto ~ Houve acerto ao apostar (0 ~ não houve aposta, 1 ~ houve aposta e acerto, 2 - houve aposta e erro)
+#contagem ~ contagem de level, se a contagem chegar a 15 o jogador sobe de level, caso a contagem chegue a -10 o jogo é perdido e level reiniciado
+#i ~ entradas
+#media5, media10, media20, media40, media80, media160, media320, media640 ~ medias das ultimas entradas. 
+entrada_inicial = {
+                    'Rodada': [0], 'level': [1], 'apostar': [0], 
+                    'acerto': [0], 'contagem': [0], 'i': [0], 
+                    'media5': [0], 'media10': [0], 'media20': [0], 
+                    'media40': [0], 'media80': [0], 'media160': [0], 
+                    'media320': [0], 'media640': [0]
+                    }
+data = pd.DataFrame(entrada_inicial)
+print(data)
 
 #Começamos o jogo aqui, com a primeira entrada gerada.
 while i != 0:
@@ -18,30 +34,36 @@ while i != 0:
     print(24*'*-')
              
     i = float(input("Insira a última entrada determinada: "))
+    acerto = 0
 #Ajustar o i_k de acordo com nossas entradas, ele deve obedecer o nosso intervalo pré-determinado.
     if apostar == 1 and i > 1.5:
         print(24*'$')
-        bet = 1
-        apostar = 0
-        win += 1
-        if win == 15:
+        acerto = 1
+        contagem += 1
+        if contagem == 15:
             level += 1
-            win = 0
-        print(f"Houve acerto! \nA ultima entrada determinada foi: {lista_entradas[-1]} \nA condição de vitoria está em {win} \nLevel: {level} \n{24*'*-'}")
+            contagem = 0
+        print(f"Houve acerto! \nA ultima entrada determinada foi: {lista_entradas[-1]} \nA condição de vitoria está em {contagem} \nLevel: {level} \n{24*'*-'}")
     if apostar == 1 and i <= 1.5:
         print(24*'$')
-        bet = 0
-        win = win - 2
-        if win <= -10:
+        acerto = 2
+        contagem = contagem - 2
+        if contagem <= -10:
             level = 1
-            win = 0 
-            print(f"Houve erro! \nA ultima entrada determinada foi: {lista_entradas[-1]} \nA condição de vitoria está em {win}, dessa maneira GAME OVER. \n{24*'*-'}")
+            contagem = 0 
+            print(f"Houve erro! \nA ultima entrada determinada foi: {lista_entradas[-1]} \nA condição de vitoria está em {contagem}, dessa maneira GAME OVER. \n{24*'*-'}")
         else:
-            print(f"Houve erro! \nA ultima entrada determinada foi: {lista_entradas[-1]} \nA condição de vitoria está em {win} \nLevel: {level} \n{24*'*-'}")
+            print(f"Houve erro! \nA ultima entrada determinada foi: {lista_entradas[-1]} \nA condição de vitoria está em {contagem} \nLevel: {level} \n{24*'*-'}")
     
     if i == 0:
         break
     lista_entradas.append(i)
+
+    if len(lista_entradas) < 6:
+        data.loc[len(data.index)] = [len(lista_entradas), level, apostar, acerto, contagem, i, 0, 0, 0, 0, 0, 0, 0, 0]
+    else:
+        data.loc[len(data.index)] = [len(lista_entradas), level, apostar, acerto, contagem, i, media5, media10, media20, media40, media80, media160, media320, media640]
+        apostar = 0
 
 #A partir da quinta entrada, essa parte do código começa a fazer o acompanhamento das médias.
     if len(lista_entradas) >= 5:
@@ -88,4 +110,8 @@ while i != 0:
             apostar = 0
 
 
+
 print(lista_entradas)
+print(data)
+
+data.to_csv('/home/darkcover/Documentos/Out/dados/data_final.csv')
