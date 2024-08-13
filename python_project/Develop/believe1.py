d01df16cff443495d4d72b245ea00c8ce9e4e4f5
Oziel Junior <58310583+oziieljuniior@users.noreply.github.com>
2024-08-13 11:28:53 -0400

    att
    No final, tudo se resume a saber lidar com as médias móveis.

--- python_project/Develop/believe1.py
+++ python_project/Develop/believe1.py
@@ -10,23 +10,11 @@ data = data.drop(columns=['Unnamed: 0'])
 data = data.rename(columns={'Odd_Categoria': 'odd_saida'})
 print("Data Carregada ...")
 
-array1, array2, array3, arraygeral = [], [], [], []
+array1, array2, array3, array4, array5,  arraygeral = [], [], [], [], [], []
 count1, count2, count3, count4 = 108, 52, 160, 320
-#Carregar funções
-def kolmogorov_complexity(data):
-    data_bytes = bytes(data)
-    compressed_data = zlib.compress(data_bytes)
-    return len(compressed_data)
 
-def martin_lof_randomness(data):
-    # Exemplo simples: Teste do qui-quadrado
-    expected = [len(data) / 2, len(data) / 2]
-    observed = [sum(data), len(data) - sum(data)]
-    chi2, p = chisquare(observed, f_exp=expected)
-    return p > 0.05  # True se passar no teste de aleatoriedade
-
-
-for i in range(len(data)):
+t1 = len(data)
+for i in range(t1):
     if data['odd_saida'][i] >= 5:
         zeroum = 1
     else:
@@ -35,34 +23,35 @@ for i in range(len(data)):
     array2.append(zeroum)
     arraygeral.append(zeroum)
     if len(array1) == 160:
-        for j in range(i, i + 160):
-            prob1 = count1/count3
-            prob2 = count2/count3
-            if data['odd_saida'][j] >= 5:
-                zeroum = 1
-            else:
-                zeroum = 0
-            array2.append(zeroum)
-            array3.append(zeroum)
-            print(f'i: {i} \nprob1: {prob1} \nprob2: {prob2} \nsaida: {zeroum}')
-            #time.sleep(0.25)
-            if zeroum == 1:
-                count1 = count1 - 1
-            else:
-                count2 = count2 - 1
-            count3 = count3 - 1
-        # Avaliação de Kolmogorov
-        kolmogorov_result = kolmogorov_complexity(array3)
-
-        # Avaliação de Martin-Löf
-        martin_lof_result = martin_lof_randomness(array3)
-        media1 = np.mean(array1)
-        media2 = np.mean(array2)
-        media3 = np.mean(array3)
-    
-        print(f'Array1: {array1} \nTamanho do array1: {len(array1)} \nMedia array1: {media1} \nArray2: {array2} \nTamanho do array2: {len(array2)} \nMedia array2: {media2} \nArray3: {array3} \nTamanho do array3: {len(array3)} \nMedia array3: {media3} \nComplexidade de Kolmogorov: {kolmogorov_result} \nPassa no teste de Martin-Löf? {martin_lof_result}')
-
-        array1, array2, array3 = [], [], []
-        
-        time.sleep(60)
-        
+        quest1 = t1 - i
+        if quest1 >= 0:
+            for j in range(i, i + 160):
+                prob1 = count1/count3
+                prob2 = count2/count3
+                if prob2 <= 0 or prob1 > 1:
+                    break
+                if data['odd_saida'][j] >= 5:
+                    zeroum = 1
+                else:
+                    zeroum = 0
+                array2.append(zeroum)
+                array3.append(zeroum)
+                if prob1 >= 0.72:
+                    array4.append(zeroum)
+                    array5.append(zeroum)
+                    media4 = np.mean(array4)
+                    media5 = np.mean(array5)
+                    print(f'j: {j} \nprob1: {prob1} \nprob2: {prob2} \nsaida: {zeroum} \nTaxa de acerto: {media4} \nTaxa de acerto local: {media5}')
+                #time.sleep(0.25)
+                if zeroum == 1:
+                    count1 = count1 - 1
+                else:
+                    count2 = count2 - 1
+                count3 = count3 - 1
+            
+            array1, array2, array3, array5 = [], [], [], []
+            count1, count2, count3 = 108, 52, 160
+print(len(array4))
+            
+            
+            
